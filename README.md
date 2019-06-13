3b6812eeef60a9856d8b930e9a8f72410f3c4567
Karen Ye <khye@wustl.edu>
2019-06-13 11:36:47 -0500

    pushed code

--- README.md
+++
@@ -1,2 +0,0 @@
-# user_alert
-Custom plugin for notifying CRM of creation of users
--- submit_test_users.php
+++
@@ -1,8 +0,0 @@
-<?php
-$path = dirname( __FILE__ ) . rand(1,100) . '.txt';
-$myfile = fopen( $path, "w" ) or die( "Unable to open file!" );
-$json = file_get_contents( 'php://input' );
-$obj = json_decode( $json );
-fwrite( $myfile, print_r( $obj, true ) );
-fclose( $myfile );
-?>
